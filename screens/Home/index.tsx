ce49ed499f9ee192d8d1b0227e2d842aacf31df4
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-31 20:55:31 -0300

    feat: project setup

---
+++ screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { Container } from './styles'
+
+export function Home () {
+  return (
+    <View style={Container}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
---
+++ screens/Home/styles.ts
@@ -0,0 +1,8 @@
+import { StyleProp, TextStyle } from "react-native";
+
+export const Container: StyleProp<TextStyle> = {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
