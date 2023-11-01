4d499e130de49c65f20166f082d4dda5adf44905
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-11-01 18:33:49 -0300

    feat: config navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { Home } from './src/screens/Home';
 import { useFonts } from 'expo-font'
 import { NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
  
 export default function App() {
   const [ fontsLoaded ] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
@@ -16,7 +16,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded && <Home /> }
+      { fontsLoaded && <Routes /> }
     </ThemeProvider>
   );
 }
