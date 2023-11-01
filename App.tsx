64f24140c85f77a8eda153f2fbe950b98779377a
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-11-01 08:39:58 -0300

    feat: config status bar

--- App.tsx
+++ App.tsx
@@ -3,12 +3,19 @@ import { useFonts } from 'expo-font'
 import { NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
+import { StatusBar } from 'react-native';
  
 export default function App() {
   const [ fontsLoaded ] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
   
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+
       { fontsLoaded && <Home /> }
     </ThemeProvider>
   );
