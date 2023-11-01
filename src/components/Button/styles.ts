5ac4f3e5308bb56ae8d87675c0e7d13e41c41e39
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-10-31 21:54:11 -0300

    style: config button styles

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,37 @@
+import { Text, TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export type ButtonStyleTypeProps = 'PRIMARY' | 'SECONDARY';
+
+export type TextStyleTypeProps = 'PRIMARY' | 'SECONDARY';
+
+type ButtonStyleProps = {
+  type: ButtonStyleTypeProps
+}
+
+type TextStyleProps = {
+  type: TextStyleTypeProps
+}
+
+export const Container = styled(TouchableOpacity)<ButtonStyleProps>`
+  ${({ theme, type }) => css`
+    background-color: ${type === 'PRIMARY' ? theme.COLORS.gray2 : theme.COLORS.gray5};
+    border: ${ type === 'PRIMARY' ? '' : `1px solid ${theme.COLORS.gray1}`};
+  `}
+   
+  min-width: 115px;
+  min-height: 50px;
+  padding: 16px 24px;
+
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+`
+
+export const TextButton = styled(Text)<TextStyleProps>`
+  ${({ theme, type }) => css`
+    font-family: ${theme.FONT_FAMILY.bold};
+    font-size: ${theme.FONT_SIZE.xsm}px;
+    color: ${type === 'PRIMARY' ? theme.COLORS.white : theme.COLORS.gray1}
+  `}
+`
