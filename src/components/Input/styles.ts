ad7f123116d97bcf39a5503c826377f3ec89bbec
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-11-01 08:40:15 -0300

    style: config input style

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,24 @@
+import { TextInput, TextInputProps } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export type InputStyleFilledProps = TextInputProps & {
+  filled: boolean
+}
+
+export const Container = styled(TextInput)<InputStyleFilledProps>`
+  align-items: flex-start;
+  padding: 14px;
+
+  width: 100%;
+  border-radius: 6px;
+
+  border: ${({ theme, filled }) => `1px solid ${filled ? theme.COLORS.gray3 : theme.COLORS.gray5 }`};
+`;
+
+export const InputText = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${ theme.FONT_FAMILY.regular };
+    color: ${ theme.COLORS.gray1 };
+    font-size: ${theme.FONT_SIZE.medium}px;
+  `}
+`
