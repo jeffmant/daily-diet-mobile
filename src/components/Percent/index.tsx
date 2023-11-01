c50b8a7efe15680e188f8930d55ebecafc393bf2
Jefferson Mantovani <jefferson.mantovani@bolttech.io>
2023-11-01 10:06:51 -0300

    feat: create percent component

---
+++ src/components/Percent/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Icon, Subtitle, TextContainer, Title } from "./styles";
+
+export function Percent () {
+  return (
+    
+    <>
+    <Container>
+        <Title>90,86%</Title>
+        <Subtitle>das refeições dentro da dieta</Subtitle>
+      <Icon />
+    </Container>
+    
+    </>
+  )
+}
